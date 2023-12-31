c73fb6341b3808bbb54e8c8603ec86e0f7327c96
DhuySoS <154112386+DhuySoS@users.noreply.github.com>
2023-12-31 15:16:24 +0700

    add tuan32

--- bai_tap_nop/app/src/main/java/com/example/bai_tap_nop/tuan22_list_view/kotlinListViewMainActivity.kt
+++ bai_tap_nop/app/src/main/java/com/example/bai_tap_nop/tuan22_list_view/kotlinListViewMainActivity.kt
@@ -12,7 +12,7 @@ class kotlinListViewMainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin_list_view_main)
         lv = findViewById(R.id.klListView);
-        var arr = arrayOf("thu 2","thu 3","thu 4","thu 5","thu 6","thu 7","chu nhat",)
+        var arr = arrayOf("thu 2","thu 3","thu 4","thu 5","thu 6","thu 7","chu nhat")
         var adapter = ArrayAdapter(this,
             android.R.layout.activity_list_item,arr)
         lv!!.adapter = adapter
